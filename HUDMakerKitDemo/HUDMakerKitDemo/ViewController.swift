fe557c9d0a62519ccd15327b36dfd74109afffc3
natmark <natmark0918@gmail.com>
2018-09-08 22:18:02 +0900

    Add HUDMakerKitDemo

---
+++ HUDMakerKitDemo/HUDMakerKitDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HUDMakerKitDemo
+//
+//  Created by AtsuyaSato on 2018/09/08.
+//  Copyright © 2018年 Atsuya Sato. All rights reserved.
+//
+
+import UIKit
+import HUDMakerKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
