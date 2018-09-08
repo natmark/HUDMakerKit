b1bfed5dc298aea5d8e30ff16dd47272617da378
natmark <natmark0918@gmail.com>
2018-09-08 21:38:38 +0900

    Add xcode proj

---
+++ HUDMakerKit/HUDMakerKit.h
@@ -0,0 +1,19 @@
+//
+//  HUDMakerKit.h
+//  HUDMakerKit
+//
+//  Created by AtsuyaSato on 2018/09/08.
+//  Copyright © 2018年 Atsuya Sato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HUDMakerKit.
+FOUNDATION_EXPORT double HUDMakerKitVersionNumber;
+
+//! Project version string for HUDMakerKit.
+FOUNDATION_EXPORT const unsigned char HUDMakerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HUDMakerKit/PublicHeader.h>
+
+
